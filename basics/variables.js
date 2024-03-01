2ef2eae571cf1c98d131244dcd658b39a7b011a4
ishajain03 <126412510+ishajain03@users.noreply.github.com>
2024-03-01 07:28:50 +0000

    container config

--- basics/variables.js
+++ basics/variables.js
@@ -3,6 +3,10 @@ var accountEmail = "ishajain@google.com"
 let accountPassword = "129876"
 accountCity = "jaipur"
 
+/*
+prefer not to use ver
+because of issue in block scope and functional scope
+*/
 accountEmail = "huu@byuv.com"
 accountPassword = "876543"
 accountCity = "modinagar"
