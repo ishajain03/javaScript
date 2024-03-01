137cccbb6c2336313039d8905da970b8ab6efced
ishajain03 <126412510+ishajain03@users.noreply.github.com>
2024-03-01 07:23:58 +0000

    container config

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 123456
+var accountEmail = "ishajain@google.com"
+let accountPassword = "129876"
+accountCity = "jaipur"
+
+accountEmail = "huu@byuv.com"
+accountPassword = "876543"
+accountCity = "modinagar"
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
