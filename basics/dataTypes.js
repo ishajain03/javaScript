84a54d295f6a5326adf99b7352ad0e9f27ee8ff2
ishajain03 <126412510+ishajain03@users.noreply.github.com>
2024-03-01 07:58:36 +0000

    container config

---
+++ basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(okh) // we are using node.js not browser
+ let age = 23
+ let name = "isha"
+
+/* 
+PRIMITIVE DATA TYPES
+1 number
+2 bigint 
+3 boolean
+4 null => standalone value nor 0
+5 undefined
+6 symbol => unique
+*/
+
+/*
+NON PRIMITIVE DATA TYPES
+1 object
+
+*/
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
