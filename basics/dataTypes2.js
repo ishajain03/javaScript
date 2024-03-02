f387b62ebf1bf9cfce5e03537cb5108788633d69
ishajain03 <126412510+ishajain03@users.noreply.github.com>
2024-03-02 07:54:22 +0000

    data type 2

---
+++ basics/dataTypes2.js
@@ -0,0 +1,25 @@
+// arrays, objects, functions
+
+
+const heores = ["shaktiman, captain america, iron men"]     // type of arrays is object
+
+console.log(typeof (heores));
+
+
+let myObj = {          // type of object is object
+    name: "isha",
+    age:    23,
+    city: "modinagar",
+    state: "uttar pradesh"
+}
+
+console.table(myObj)
+console.log(typeof myObj);
+console.table(heores)
+console.log(typeof heores);
+
+const myFunction = function(){       // type of function is object function
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
