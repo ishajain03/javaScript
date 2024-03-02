5afb442669352e7d54c63f1641162907e9e0e731
ishajain03 <126412510+ishajain03@users.noreply.github.com>
2024-03-02 13:00:20 +0000

    more about arrays

---
+++ basics/Arrays2.js
@@ -0,0 +1,23 @@
+const dc_heros = ["superman", "flash", "batman"]
+const marvel_heros = ["captain america", "iron man", "vision"]
+// marvel_heros.push(dc_heros)
+ console.log(marvel_heros);
+
+ const allHeros = marvel_heros.concat(dc_heros)
+ console.log(allHeros);
+
+ // SPREAD OPERATOR 
+
+ const all_new_heros = [...marvel_heros, ...dc_heros]
+ console.log(all_new_heros);
+
+
+ const anotherArray = [1,2,3,[4,5,6],7,[6,7,[3,4,5,6]]]
+
+ const realAnotherArray = anotherArray.flat(Infinity)
+
+ console.log(realAnotherArray);
+
+
+console.log(Array.isArray('isha'));
+console.log(Array.from("isha"));
